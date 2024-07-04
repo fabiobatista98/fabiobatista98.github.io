74a750944ed714989ce2ab745ce69be0696861cb
fabiobatista98 <fabio.s.baptista98@gmail.com>
2024-07-04 11:27:23 +0100

    Add certifications and fix titles of work experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,46 @@
 		});
 
 })(jQuery);
+
+let slideIndex = 1;
+let slideInterval;
+
+showSlides(slideIndex);
+startSlideShow();
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  resetSlideShow();
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  resetSlideShow();
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function startSlideShow() {
+  slideInterval = setInterval(function() {
+    plusSlides(1);
+  }, 5000);
+}
+
+function resetSlideShow() {
+  clearInterval(slideInterval);
+  startSlideShow();
+}
